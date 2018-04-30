e71360acdc738c617b08901f45a7cbd7409fe587
Jeff Jarvis <jjarvis@codehatlabs.com>
2018-04-30 14:00:32 -0700

    add on_hashids_decoded method

--- django_hashids/views.py
+++ django_hashids/views.py
@@ -5,9 +5,9 @@ class DjangoHashidsViewMixin(object):
 
     hidargs = {}
 
-    def decode_hashids(self, hids, kwargs):
+    def decode_hashids(self, request, kwargs):
         for argkey, argtype in self.hidargs.items():
-            arg = hids.decode(kwargs[argkey])[0]
+            arg = request.hids.decode(kwargs[argkey])[0]
             if argtype == 'uuid':
                 arg = UUID(int=arg)
             elif argtype == 'uuidhex':
@@ -15,9 +15,13 @@ class DjangoHashidsViewMixin(object):
             elif argtype == 'uuidstr':
                 arg = str(UUID(int=arg))
             kwargs[argkey] = arg
+        self.on_hashids_decoded(request, kwargs)
 
     def dispatch(self, request, *args, **kwargs):
-        self.decode_hashids(request.hids, kwargs)
+        self.decode_hashids(request, kwargs)
         return super().dispatch(request, *args, **kwargs)
+
+    def on_hashids_decoded(self, request, kwargs):
+        pass
 
 
